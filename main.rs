ad1b9e573a1c2cafd769a06ddbf78b4af5f3a96c
unknown <A230926AC@rvt.lv>
2025-05-07 21:21:10 +0300

    snake

--- main.rs
+++ main.rs
@@ -1,6 +1,20 @@
-mod test_projects;
-use test_projects::guess_the_num::run_game;
+use std::io;
+
+fn input() -> String {
+    let mut inp = String::new();
+    io::stdin().read_line(&mut inp).expect("error reading input");
+    inp.trim().to_string()
+}
+
+struct CLI {
+    command: String,
+    flag1: 
+}
+
+
 fn main() {
-    println!("running module...");
-    run_game();
+    let command = input();
+    if command.contains(&"cab ".to_string()) {
+        println!("{}", &command[4..]);
+    }
 }
---
+++ snake/main.rs
@@ -0,0 +1,12 @@
+extern crate rand;
+extern crate piston_window;
+
+mod draw;
+mod snake;
+mod game;
+
+
+
+fn main() {
+    println!("I FUCKING HATE FUCKIN NIGGERS");
+}
